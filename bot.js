5a7b31df4bef115b38ebe7c36bee9691088ad264
AhmedMarwan1023 <44772647+AhmedMarwan1023@users.noreply.github.com>
2019-05-03 21:41:30 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+
+client.on('ready', () => {
+console.log(`Online.`);
+ });
+ 
+ client.on("message", message => {
+    if(message.content.startsWith(prefix + "تفعيل")){
+        message.channel.send("**اضغـط للتـفعيـل**").then(m => {
+m.react('الرياكشن')
+let reaction1Filter = (reaction, user) => reaction.emoji.name === 'الرياكشن' && user.id === message.author.id;
+let reaction1 = m.createReactionCollector(reaction1Filter, { time: 19000 });
+reaction1.on("collect", r => {
+let role = message.guild.roles.find("name", "Actived"); //Actived تقدر تغير الرول حق التفعيل انا مسميه
+message.member.addRole(role)
+})
+})
+    }
+})
+
+client.login(process.env.BOT_TOKEN);
